99dc1fe508bd5632d0cf3b0ea2e23266e858cdaa
yeomfa <yeomfa@gmail.com>
2026-02-19 18:08:49 -0500

    Implement Member entity and improve validation logic

--- apps/backend/src/domain/entities/Member.js
+++ apps/backend/src/domain/entities/Member.js
@@ -1,11 +1,28 @@
 export class Member {
-  id;
-  identificationNumber;
-  identificationType;
-
   constructor({ id, identificationNumber, identificationType }) {
     this.id = id;
     this.identificationNumber = identificationNumber;
     this.identificationType = identificationType;
+    this.validate();
+  }
+
+  validate() {
+    if (!this.id) {
+      throw new Error('Member ID is required');
+    }
+    if (!this.identificationNumber) {
+      throw new Error('Identification number is required');
+    }
+    if (!this.identificationType) {
+      throw new Error('Identification type is required');
+    }
+  }
+
+  update(newData) {
+    this.identificationNumber =
+      newData.identificationNumber || this.identificationNumber;
+    this.identificationType =
+      newData.identificationType || this.identificationType;
+    this.validate();
   }
 }
